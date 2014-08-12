2921285d988830f869dfca23199e908118a5aeee
Karl Ehatäht <karlehataht@gmail.com>
2014-08-12 15:41:14 +0200

    Fixed combinations.cpp.

--- src/combinations.cpp
+++ src/combinations.cpp
@@ -6,7 +6,8 @@
 #include <vector> // std::vector
 #include <cstdlib> // EXIT_SUCCESS
 #include <numeric> // std::accumulate
- 
+
+
 float comb(std::vector<float> & v, int N, int K) {
 	std::string bitmask(K, 1); // K leading 1's
 	bitmask.resize(N, 0); // N-K trailing 0's
@@ -15,7 +16,9 @@ float comb(std::vector<float> & v, int N, int K) {
 		float prob = 1;
 		for (int i = 0; i < N; ++i) {
 			if (bitmask[i]) prob *= v[i];
+			else prob *= (1 - v[i]);
 		}
+		std::cout << prob << std::endl;
 		sum_prob += prob;
 	} while (std::prev_permutation(bitmask.begin(), bitmask.end()));
 	return sum_prob;
@@ -27,14 +30,16 @@ int main(void) {
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 	std::mt19937_64 gen(seed);
 	std::uniform_real_distribution<float> dis(0,1);
+	
 	std::vector<float> v;
 	for(int i = 1; i <= Nj; ++i) v.push_back(dis(gen));
-	float sum = std::accumulate(v.begin(), v.end(), 0.0);
-	std::transform(v.begin(), v.end(), v.begin(), [sum] (float x) -> float {
-		return x / sum;
-	});
+	
+	std::cout << "individual probabilities:" << std::endl;
 	for(auto val: v) std::cout << std::fixed << val << "\t";
-	std::cout << std::endl << "combined probability: " << comb(v, Nj, Ntag) << std::endl;
+	std::cout << std::endl << std::endl << "combined probabilities:" << std::endl;
+	float combi = comb(v, Nj, Ntag);
+	std::cout << std::endl << "the sum of combined probability: \t";
+	std::cout << combi << std::endl;
 	
 	return EXIT_SUCCESS;
 }
