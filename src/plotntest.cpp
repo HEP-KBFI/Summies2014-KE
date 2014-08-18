6e49516924b22b0d240703ced7c6074f26b28009
Karl Ehatäht <karlehataht@gmail.com>
2014-08-18 11:55:58 +0200

    plotntest.cpp is now able to carry out some tests.

--- src/plotntest.cpp
+++ src/plotntest.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib> // EXIT_SUCCESS, std::exit
 #include <iostream> // std::cout, std::cerr, std::endl
 #include <fstream> // std::ofstream
+#include <sstream> // std::stringstream
 #include <streambuf> // std::streambuf
 #include <map> // std::map<>
 #include <string> // std::string
@@ -27,7 +28,7 @@ int main(int argc, char ** argv) {
 	
 	std::string input, output, dir, extension;
 	Int_t dimX, dimY;
-	bool setLog = false;
+	bool setLog = false, writeToFile = false, doPlots = false, doKolmogorov = false, doChi2 = false;
 	try {
 		po::options_description desc("allowed options");
 		desc.add_options()
@@ -36,7 +37,10 @@ int main(int argc, char ** argv) {
 			("dimx,x", po::value<Int_t>(&dimX) -> default_value(900), "the x dimension of the histogram")
 			("dimy,y", po::value<Int_t>(&dimY) -> default_value(600), "the y dimension of the histogram")
 			("dir,d", po::value<std::string>(&dir), "the output directory")
-			("extension,e", po::value<std::string>(&extension), "extension of the output files")
+			("extension,e", po::value<std::string>(&extension), "extension of the output files\nif not set, no plots will be created")
+			("output,o", po::value<std::string>(&output), "output file\nif not specified, printed to stdout")
+			("kolmogorov,k", "do Kolmogorov test")
+			("chi2,c", "do chi 2 test")
 			("enable-log,l", "sets y-axis to logarithmic scale")
 		;
 		
@@ -48,12 +52,24 @@ int main(int argc, char ** argv) {
 			std::cout << desc << std::endl;
 			std::exit(EXIT_SUCCESS); // ugly
 		}
-		if(vm.count("input") == 0 || vm.count("extension") == 0) {
+		if(vm.count("input") == 0) {
 			std::cout << desc << std::endl;
 			std::exit(EXIT_SUCCESS);
 		}
 		if(vm.count("enable-log") > 0) {
 			setLog = true;
+		}
+		if(vm.count("extension") > 0) {
+			doPlots = true;
+		}
+		if(vm.count("output") > 0) {
+			writeToFile = true;
+		}
+		if(vm.count("kolmogorov") > 0) {
+			doKolmogorov = true;
+		}
+		if(vm.count("chi2") > 0) {
+			doChi2 = true;
 		}
 	}
 	catch(std::exception & e) {
@@ -62,6 +78,10 @@ int main(int argc, char ** argv) {
 	}
 	catch(...) {
 		std::cerr << "exception of unkown type" << std::endl;
+	}
+	if(! (doPlots || doKolmogorov || doChi2)) {
+		std::cerr << "at least one of the following flags are required: -e -k -c" << std::endl;
+		std::exit(EXIT_FAILURE);
 	}
 	
 	TFile * inFile = TFile::Open(input.c_str(), "read");
@@ -105,47 +125,93 @@ int main(int argc, char ** argv) {
 	histoXaxis["eta"] = "Jet #eta";
 	histoXaxis["csv"] = "Jet CSV";
 	
-	for(auto & kv: vars) {
-		auto s = kv.first;
-		TCanvas * c = new TCanvas(histoTitles[s].c_str(), histoTitles[s].c_str(), dimX, dimY);
-		gStyle -> SetOptStat(kFALSE);
-		TLegend * legend = new TLegend(0.60, 0.90, 0.95, 0.95);
-		legend -> SetNColumns(vars[s]);
-		int counter = 0;
-		for(auto & h: histoMap) {
-			std::string title = h.first;
-			if(boost::iequals(title.substr(0, title.find(" ")), s)) {
-				std::string suffix = title.substr(title.find(s) + s.size() + 1);
-				h.second -> SetLineColor(colorRanges[counter]);
-				h.second -> SetLineWidth(2);
-				h.second -> SetLineWidth(2);
-				
-				h.second -> GetXaxis() -> SetTitle(histoXaxis[s].c_str());
-				h.second -> GetXaxis() -> SetTitleOffset(1.2);
-				h.second -> GetYaxis() -> SetTitle("Number of events per bin");
-				h.second -> GetYaxis() -> SetTitleOffset(1.2);
-				h.second -> SetMinimum(0);
-				h.second -> Draw((counter == 0 ? "hist e" : "same hist e")); // same e for the error bars
-				h.second -> SetTitle(histoTitles[s].c_str());
-				std::string entriesString = " (" + std::to_string(Int_t(h.second -> GetEntries())) + ")";
-				legend -> AddEntry(h.second, (histoLabels[suffix] + entriesString).c_str());
-				c -> SetGrid();
-				if(setLog) c -> SetLogy(1);
-				c -> SetRightMargin(0.05);
-				c -> Modified();
-				c -> Update();
-				++counter;
+	if(doPlots) {
+		for(auto & kv: vars) {
+			auto s = kv.first;
+			TCanvas * c = new TCanvas(histoTitles[s].c_str(), histoTitles[s].c_str(), dimX, dimY);
+			gStyle -> SetOptStat(kFALSE);
+			TLegend * legend = new TLegend(0.70, 0.90, 0.95, 1.0);
+			//legend -> SetNColumns(vars[s]);
+			int counter = 0;
+			for(auto & h: histoMap) {
+				std::string title = h.first;
+				if(boost::iequals(title.substr(0, title.find(" ")), s)) {
+					std::string suffix = title.substr(title.find(s) + s.size() + 1);
+					h.second -> SetLineColor(colorRanges[counter]);
+					h.second -> SetLineWidth(2);
+					h.second -> SetLineWidth(2);
+					
+					h.second -> GetXaxis() -> SetTitle(histoXaxis[s].c_str());
+					h.second -> GetXaxis() -> SetTitleOffset(1.2);
+					h.second -> GetYaxis() -> SetTitle("Number of events per bin");
+					h.second -> GetYaxis() -> SetTitleOffset(1.2);
+					h.second -> SetMinimum(0);
+					h.second -> Draw((counter == 0 ? "hist e" : "same hist e")); // same e for the error bars
+					h.second -> SetTitle(histoTitles[s].c_str());
+					std::string entriesString = " (" + std::to_string(Int_t(h.second -> GetEntries())) + ")";
+					legend -> AddEntry(h.second, (histoLabels[suffix] + entriesString).c_str());
+					c -> SetGrid();
+					if(setLog) c -> SetLogy(1);
+					c -> SetRightMargin(0.05);
+					c -> Modified();
+					c -> Update();
+					++counter;
+				}
 			}
+			legend -> Draw();
+			std::string canvasTitle = s;
+			if(setLog) canvasTitle = "log_" + canvasTitle;
+			if(! dir.empty()) canvasTitle = dir + "/" + canvasTitle;
+			c -> SaveAs(canvasTitle.append("." + extension).c_str());
+			c -> Close();
+			delete legend;
 		}
-		legend -> Draw();
-		std::string canvasTitle = s;
-		if(setLog) canvasTitle = "log_" + canvasTitle;
-		if(! dir.empty()) canvasTitle = dir + "/" + canvasTitle;
-		c -> SaveAs(canvasTitle.append("." + extension).c_str());
-		c -> Close();
-		delete legend;
 	}
 	
+	if(doKolmogorov || doChi2) {
+		std::stringstream ss;
+		for(auto & h: histoMap) {
+			std::string title = h.first;
+			std::string varString = title.substr(0, title.find(" "));
+			std::string suffix = title.substr(title.find(" ") + 1);
+			if(boost::iequals(suffix, "H")) {
+				std::vector<TH1F *> testHistos;
+				for(auto & k: histoMap) {
+					std::string titleInner = k.first;
+					std::string varStringInner = titleInner.substr(0, titleInner.find(" "));
+					std::string suffixInner = titleInner.substr(titleInner.find(" ") + 1);
+					if(! boost::iequals(varString, varStringInner)) continue;
+					if(boost::iequals(suffixInner, "A") || boost::iequals(suffixInner, "M")) {
+						testHistos.push_back(k.second);
+					}
+				}
+				ss << varString << std::endl;
+				for(auto & k: testHistos) {
+					if(doKolmogorov) {
+						auto kTest = h.second -> KolmogorovTest(k);
+						ss << "Kolmogorov:\t" << std::fixed << kTest << std::endl;
+					}
+					if(doChi2) {
+						auto kTest = h.second -> Chi2Test(k, "UW");
+						ss << "Chi2:\t\t" << std::fixed << kTest << std::endl;
+					}
+					ss << "-----------------------------" << std::endl;
+				}
+			}
+		}
+		std::streambuf * buf;
+		std::ofstream of;
+		if(writeToFile) {
+			of.open(output);
+			buf = of.rdbuf();
+		}
+		else {
+			buf = std::cout.rdbuf();
+		}
+		std::ostream out(buf);
+		out << ss.str();
+	}
+		
 	inFile -> Close();
 	
 	return EXIT_SUCCESS;
